839a24fc83d357a584bf66a0c92ee676b7f68ccd
tqa3001 <tqa3001@gmail.com>
2023-02-02 18:37:40 +0700

    change CA color

--- src/Components/CAEntry.jsx
+++ src/Components/CAEntry.jsx
@@ -1,6 +1,6 @@
 export default function CAEntry({ automaton, styleOptions }) {
   return (
-    <div className={"mx-2 my-1 p-2 bg-blue-500 rounded-xl text-white hover:text-yellow-400 hover:bg-blue-900" + " " + styleOptions}>
+    <div className={"mx-2 my-1 p-2 bg-blue-500 rounded-xl text-white hover:text-yellow-300 hover:bg-blue-700" + " " + styleOptions}>
       <span className="font-bold">#{automaton.id} | </span>
       <span className="font-bold">{automaton.name} | </span>
       <span>{automaton.shortDescription}</span>
